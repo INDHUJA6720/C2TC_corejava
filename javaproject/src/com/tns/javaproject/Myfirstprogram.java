42d4e829820ac57d90f65de81d51769ade8b8664
Lenovo <Lenovo@DESKTOP-3NDNV5Q>
2022-09-07 03:10:14 -0700

    First commit

---
+++ javaproject/src/com/tns/javaproject/Myfirstprogram.java
@@ -0,0 +1,14 @@
+package com.tns.javaproject;
+
+public class Myfirstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a = 'C';
+		int n = a+4;
+		char ch = (char)n;
+		System.out.println(ch);
+
+	}
+
+}
